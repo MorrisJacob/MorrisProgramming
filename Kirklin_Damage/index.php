723d66be56c50acedd235fc6fa3003c68116ea46
Jacob Morris <jacob.morris@agreliant.com>
2023-07-07 14:26:45 -0400

    Added 404 page; damage zip files

---
+++ Kirklin_Damage/index.php
@@ -0,0 +1,29 @@
+<html>
+<ul>
+<li><a href="HailDamage.zip">Hail Damage</a></li>
+<li><a href="HailPieces.zip">Hail Pieces</a></li>
+<li><a href="TreeDamage.zip">Tree Damage</a></li>
+<img src="images/Hail1.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Hail2.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Hail3.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Hail4.png" style="width:300px;padding:10px;"/>
+<img src="images/Hail5.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Roof1.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Roof2.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Roof3.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Roof4.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Roof5.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image7.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image8.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image9.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image5.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image4.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image3.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image2.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image1.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image4-7.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image4-8.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/image4-9.jpeg" style="width:300px;padding:10px;"/>
+<img src="images/Roof10.jpeg" style="width:300px;padding:10px;"/>
+</ul>
+</html>
